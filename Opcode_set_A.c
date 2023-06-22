be1201617e9d394142b50ffc30aee8bf496708e1
KevCareSA <kev.mkbn2@gmail.com>
2023-06-22 17:11:08 +0200

    error update2

--- Opcode_set_A.c
+++ Opcode_set_A.c
@@ -37,7 +37,7 @@ void mont_pall(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 	if (!node_iterator)
 	{
-		fprintf(stderr, "Error: malloc operation failed :(.\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
